8e2ab053bc7c8b1707d6dba4c149d09c01c7e4ee
ma3atr0n <ma3atr0n@gmail.com>
2022-04-12 23:59:43 +0300

    Добавил часть 12-ого задания

--- js/main.js
+++ js/main.js
@@ -3,8 +3,15 @@ import './picture-upload.js';
 import './validate.js';
 import {createGetLoader} from  './data-loader.js';
 import {showAlert} from './utils.js';
+import {createFilter} from './filter.js';
 
 const URL_GET = 'https://25.javascript.pages.academy/kekstagram/data';
 
-createGetLoader(URL_GET, addPictures, showAlert);
+createGetLoader(URL_GET,
+  (data) => {
+    addPictures(data);
+    createFilter(data);
+  },
+  showAlert
+);
 
--- js/pictures.js
+++ js/pictures.js
@@ -2,8 +2,11 @@ import {openBigPicModal} from './big-picture.js';
 
 const addPictures = (photosArray) => {
 
-  //Находим секцию с картинками, секуцию с большими картинками и кнопку закрытия окна (для передачит на открытие окна)
+  //Находим секцию с картинками, секуцию с большими картинками и все равнее добавленные картинки
   const pictures = document.querySelector('.pictures');
+  const picturesElement = document.querySelectorAll('.pictures .picture');
+  //Удаляем ранее загруженные картинки
+  picturesElement.forEach((element) => element.remove());
   //Находим темплейт для добавления картинок
   const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
   //Создаем фрагмен для единовременного добавления всех картинок из массива
